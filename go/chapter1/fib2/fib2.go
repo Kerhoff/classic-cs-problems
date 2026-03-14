5deae6064ef01326f6ba7506840bbe39ffcce370
Aleksandr Lukashkin <aslukashkin@nebius.com>
2026-03-14 14:17:52 +0100

    fib2 - go

---
+++ go/chapter1/fib2/fib2.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func fib2(n int) int {
+	if n < 2 {
+		return n
+	}
+
+	return fib2(n-2) + fib2(n-1)
+}
+
+func main() {
+	fmt.Println(fib2(5))
+	fmt.Println(fib2(10))
+}
